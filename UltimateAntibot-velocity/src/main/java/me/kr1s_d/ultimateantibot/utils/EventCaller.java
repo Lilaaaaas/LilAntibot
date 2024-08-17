0cc59b27d9021cbe3d69ddba0a69b5455f49b992
Lilaaaaas <overwaul9@gmail.com>
2024-08-17 13:38:30 -0700

    Delete UltimateAntibot-velocity directory

--- UltimateAntibot-velocity/src/main/java/me/kr1s_d/ultimateantibot/utils/EventCaller.java
+++
@@ -1,9 +0,0 @@
-package me.kr1s_d.ultimateantibot.utils;
-
-import me.kr1s_d.ultimateantibot.UltimateAntiBotVelocity;
-
-public class EventCaller {
-    public static void call(Object event) {
-        UltimateAntiBotVelocity.getInstance().getServer().getEventManager().fire(event);
-    }
-}
